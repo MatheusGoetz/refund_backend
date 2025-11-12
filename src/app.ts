d9085c1bb895ce01d836b40ac74ccb9156639063
Matheus Goetz <goetz.dev@gmail.com>
2025-11-12 18:29:36 -0300

    feat:implementado middleware de autenticação de acesso

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import "express-async-errors";
 import cors from "cors";
 
 import { errorHandling } from "./middlewares/error-handling";
+import { routes } from "./routes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(routes);
 
 app.use(errorHandling);
 
