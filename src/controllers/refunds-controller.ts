ba7c4989a11e735bf63fba57afa2ec8e176ec1ed
Matheus Goetz <goetz.dev@gmail.com>
2025-11-16 20:12:03 -0300

    feat: implementado paginação

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,8 +1,92 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
+
+const CategoriesEnum = z.enum([
+	"food",
+	"others",
+	"services",
+	"transport",
+	"accomodation",
+]);
 
 class RefundsController {
 	async create(req: Request, res: Response) {
-		res.json({ message: "Ok" });
+		const bodySchema = z.object({
+			name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+			category: CategoriesEnum,
+			amount: z.number().positive({ message: "O valor precisa ser positivo" }),
+			filename: z.string().min(20),
+		});
+
+		const { name, category, amount, filename } = bodySchema.parse(req.body);
+
+		if (!req.user?.id) {
+			throw new AppError("Não Autorizado", 401);
+		}
+
+		const refund = await prisma.refunds.create({
+			data: {
+				name,
+				category,
+				amount,
+				filename,
+				userID: req.user.id,
+			},
+		});
+
+		res.status(201).json(refund);
+	}
+
+	async index(req: Request, res: Response) {
+		const querySchema = z.object({
+			name: z.string().optional().default(""),
+			page: z.coerce.number().optional().default(1),
+			perPage: z.coerce.number().optional().default(10),
+		});
+
+		const { name, page, perPage } = querySchema.parse(req.query);
+
+		const skip = (page - 1) * perPage;
+
+		const refunds = await prisma.refunds.findMany({
+			skip,
+			take: perPage,
+			where: {
+				user: {
+					name: {
+						contains: name.trim(),
+					},
+				},
+			},
+			orderBy: { createdAt: "desc" },
+			include: {
+				user: true,
+			},
+		});
+
+		const totalRecords = await prisma.refunds.count({
+			where: {
+				user: {
+					name: {
+						contains: name.trim(),
+					},
+				},
+			},
+		});
+
+		const totalPages = Math.ceil(totalRecords / perPage);
+
+		res.json({
+			refunds,
+			pagination: {
+				page,
+				perPage,
+				totalRecords,
+				totalPages: totalPages > 0 ? totalPages : 1,
+			},
+		});
 	}
 }
 
